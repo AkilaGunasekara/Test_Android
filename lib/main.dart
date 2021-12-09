c10332ea8502768bf93bec226c7ac64ec888925d
AkilaGunasekara <cst18022@std.uwu.ac.lk>
2021-12-09 13:07:37 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,68 +2,67 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-    builder: (context) => MyApp(), // Wrap your app
-  ),
-    
-    );
+  runApp(RecipeApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class RecipeApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      title: 'Flutter Demo',
-      theme: ThemeData(
- 
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title: const Text('Flutter') ,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
-        ),
-        actions: <Widget>[
-          IconButton(
-          icon: Icon(Icons.search_rounded),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: Icon(Icons.more_vert_rounded),
-          onPressed: () {},
-        ), 
-        ],
-        flexibleSpace: Image.asset(
-          "assests/photo.jpg",
-          fit: BoxFit.none,
-          colorBlendMode:BlendMode.softLight,
-          
+      // 2
+      final ThemeData theme = ThemeData();
+      // 3
+      return MaterialApp(
+        // 4
+        title: 'Recipe Calculator',
+        // 5
+        theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.grey,
+            secondary: Colors.black,
           ),
-          bottom: PreferredSize(
-            child: Container(),
-            preferredSize: Size.fromHeight(70.0),
-            ),
-
-      ),
-
-      
-    );
+        ),
+        // 6
+      );
+    }
   }
-}
+
+//
+// class MyHomePage extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar:AppBar(
+//         title: const Text('Flutter') ,
+//         leading: IconButton(
+//           icon: Icon(Icons.menu),
+//           onPressed: () {},
+//         ),
+//         actions: <Widget>[
+//           IconButton(
+//           icon: Icon(Icons.search_rounded),
+//           onPressed: () {},
+//         ),
+//         IconButton(
+//           icon: Icon(Icons.more_vert_rounded),
+//           onPressed: () {},
+//         ),
+//         ],
+//         flexibleSpace: Image.asset(
+//           "assests/photo.jpg",
+//           fit: BoxFit.none,
+//           colorBlendMode:BlendMode.softLight,
+//
+//           ),
+//           bottom: PreferredSize(
+//             child: Container(),
+//             preferredSize: Size.fromHeight(70.0),
+//             ),
+//
+//       ),
+//
+//
+//     );
+//   }
+// }
